75e325d4ab2b1001e66f0db6a34af5f4dbbcfb9d
Marcelo Mazon <marcelo.tmazon@gmail.com>
2026-03-07 22:36:03 -0300

    services data

--- app.js
+++ app.js
@@ -1,7 +1,4 @@
-window.addEventListener("DOMContentLoaded", () => {
-  const nav = document.querySelector("nav");
-  console.log(nav);
-  nav.innerHTML = `
-  <h2>Hello DOM</h2>
-  <p>This is HTML within a JS string</p>`;
-});
+import API from "./services/API";
+import Store from "./services/Store";
+
+window.addEventListener("DOMContentLoaded", () => {});
