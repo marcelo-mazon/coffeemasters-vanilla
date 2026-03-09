9e6ff72f783d0c2056244b452dc9408a3e429ecc
Marcelo Mazon <marcelo.tmazon@gmail.com>
2026-03-08 22:03:06 -0300

    routing

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
-import API from "./services/API";
-import Store from "./services/Store";
+import Store from "./services/Store.js";
+import { loadData } from "./services/Menu.js";
+import Router from "./services/Router.js";
 
-window.addEventListener("DOMContentLoaded", () => {});
+window.app = {};
+app.store = Store;
+app.router = Router;
+
+window.addEventListener("DOMContentLoaded", async () => {
+  loadData();
+  app.router.init();
+});
